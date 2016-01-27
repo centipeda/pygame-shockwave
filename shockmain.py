22f4860e738aed9c05f8de4c3b78202e490d56a0
j-crit <joshua.c.guam@gmail.com>
2016-01-27 21:01:33 +1000

    Initialized the pygame module, as well as adding the main display with the game loop.

--- shockmain.py
+++ shockmain.py
@@ -3,4 +3,15 @@
 Shockwave is a simple game that is based on flipping tiles in a square grid.
 """
 
-import pygame
+import pygame,sys
+pygame.init()
+
+# create window
+mainDisplay = pygame.display.set_mode((500,))
+while True: # main loop
+    # quit if window closed
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+    
+    pygame.display.update()
